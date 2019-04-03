b308905e30bb58c8d88ba9b74fe66c5d94b5f29b
Calum Darroch <calum_darroch@hotmail.com>
2019-04-03 16:47:42 +0100

    Challenge 10

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,7 +1,10 @@
 require 'DockingStation'
 
 describe DockingStation do
-  describe '#release_bike' do
     it { is_expected.to respond_to :release_bike }
-  end
+
+    it 'releases a working bike' do
+      bike = subject.release_bike
+      expect(bike).to be_working
+    end
 end
