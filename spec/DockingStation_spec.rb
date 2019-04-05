77926dc85417a7234d45620ad606d6f2417f21f1
domw30 <domw30@gmail.com>
2019-04-05 15:23:57 +0100

    add raise exception method if bikes is nil

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -2,21 +2,25 @@ require 'DockingStation'
 
 describe DockingStation do
     it { is_expected.to respond_to :release_bike }
-
-    it 'releases a working bike' do
-      bike = subject.release_bike
-      expect(bike).to be_working
-    end
-
     it { is_expected.to respond_to(:dock).with(1).argument }
-
-    # included test to identify bike method
     it { is_expected.to respond_to(:bike) }
 
-    # added unit test which expects the dock method to return the bike when passed an argument of bike
+    describe '#release_bike' do
+  		it "should create a new Bike" do
+  		  docking_station = subject
+  			docking_station.dock(Bike.new)
+  			expect(docking_station.release_bike).to be_working
+  		end
+  		it "should print a message when no bikes" do
+  			expect{subject.release_bike}.to raise_error "No bike available"
+  		end
+  	end
+
+  describe '#dock' do
     it 'returns docked bikes' do
       bike = Bike.new
       subject.dock(bike)
       expect(subject.bike).to eq bike
     end
+  end
 end
